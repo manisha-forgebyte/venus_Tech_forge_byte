4a25fd633ee5f2dd599ad58def6ac0d459bf85f0
manisha-forgebyte <manisha.kakarla@forgebyte.ai>
2026-06-17 10:40:23 +0530

    Update production baseUrl for API endpoint

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: '/api',
+    baseUrl: 'https://venustechforgebyte-production.up.railway.app/api',
     fercMBRValidation: 'http://mbrweb.ferc.gov/MBRValidationXML/{0}/MBRValidation_{0}.xml',
     fercMBRAPIBase: 'http://mbrwebapi.ferc.gov/'
 };
